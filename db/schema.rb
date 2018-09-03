6ce4c50cc52fcb0179a8c0c0d7b937283c1ff602
Lesley Hogg <lesleyhogg@gmail.com>
2018-09-02 21:36:57 -0400

    Added associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_03_20_022457) do
+ActiveRecord::Schema.define(version: 2015_04_09_201335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "pins", id: :serial, force: :cascade do |t|
     t.string "title"
     t.string "url"
     t.text "text"
     t.string "slug"
-    t.string "resource_type"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_pins_on_category_id"
   end
 
 end
