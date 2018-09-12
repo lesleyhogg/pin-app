ce793ccdf97fe0d0d3a1b21cab2bf199a2e352a4
Lesley Hogg <lesleyhogg@gmail.com>
2018-09-11 22:45:07 -0400

    added form helper

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -13,4 +13,43 @@ class PinsController < ApplicationController
     render :show
   end
 
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.create(pin_params)
+
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @errors = pin.errors
+      render :new
+    end
+  end
+
+  def edit
+    @pin = Pin.find(params[:id])
+  end
+
+  def update
+    @pin = Pin.find(params[:id])
+    @pin.update(pin_params)
+
+    if @pin.valid?
+      @pin.save
+      redirect_to pin_path(@pin)
+    else
+      @errors = pin.errors
+      render :edit
+    end
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :url, :slug, :text, :category_id)
+  end
+
 end
